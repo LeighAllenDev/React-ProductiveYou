342a030f7bd96a50945286915413749a2a69d0d1
Leigh Allen <lallenation@gmail.com>
2024-07-06 20:59:25 +0000

    fix: fix import method

--- my-app/src/hooks/ClickOutsideToggle.js
+++ my-app/src/hooks/ClickOutsideToggle.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react';
 
 const UseClickOutsideToggle = () => {
     const [expanded, setExpanded] = useState(false);

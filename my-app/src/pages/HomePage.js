2adf69198f7336969e7da9d2a8ee237c02eca765
Leigh Allen <lallenation@gmail.com>
2024-07-09 22:06:22 +0000

    Add text to homepage

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -4,6 +4,7 @@ const HomePage = () => {
   return (
     <div>
         <h1>Welcome to Productive You!</h1>
+        <p>This is a productivity App where users can join teams, edit their profiles, make categories and add tasks.<br></br>It provides the ability to assign tasks to other users so everyone can contribute!</p>
     </div>
   )
 }

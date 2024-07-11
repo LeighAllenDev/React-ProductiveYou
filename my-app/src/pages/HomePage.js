90576f336c4e59573d97c17b02a45d550efa9a34
Leigh Allen <lallenation@gmail.com>
2024-07-11 21:06:35 +0000

    feat: Add styling and image to home page

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import hero from "../assets/hero.jpg";
+import appStyles from "../styles/App.module.css";
 
 const HomePage = () => {
   return (
-    <div>
-        <h1>Welcome to Productive You!</h1>
-        <p>This is a productivity App where users can join teams, edit their profiles, make categories and add tasks.<br></br>It provides the ability to assign tasks to other users so everyone can contribute!</p>
-    </div>
-  )
-}
+    <Container className={appStyles.App}>
+      <Row className="justify-content-center text-center mt-5">
+        <Col>
+          <h1>Welcome to Productive You!</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-center text-center mt-3">
+        <Col>
+          <p>This is a productivity App where users can join teams, 
+            edit their profiles, make categories and add tasks.<br />
+            It provides the ability to assign tasks to other users
+            so everyone can contribute!</p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center text-center mt-5">
+        <Col>
+          <img className={`${appStyles.FillerImage}`} src={hero} alt="Hero" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;

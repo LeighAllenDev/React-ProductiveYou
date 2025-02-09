94a6a4015e42d9cdb70b0b36e5fbe1ee11a29ccd
Leigh Allen <lallenation@gmail.com>
2025-02-09 21:14:35 +0000

    fix: update axios

--- my-app/src/api/axiosDefaults.js
+++ my-app/src/api/axiosDefaults.js
@@ -1,55 +1,81 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// Base URL and default headers setup
+// Base API URL
 axios.defaults.baseURL = 'https://productive-you-api-d9afbaf8a80b.herokuapp.com';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
 
-const csrftoken = Cookies.get('csrftoken');
-if (csrftoken) {
-  axios.defaults.headers.common['X-CSRFToken'] = csrftoken;
-}
+// Function to get CSRF token dynamically
+const getCSRFToken = () => Cookies.get('csrftoken');
 
+// Create axios instances
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
 
-axiosReq.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response && error.response.status === 401) {
-      try {
-        const refreshedToken = await refreshToken();
-        const config = error.config;
-        config.headers['Authorization'] = `Bearer ${refreshedToken.access}`;
-        return axiosReq(config);
-      } catch (refreshError) {
-        console.error('Error refreshing token:', refreshError);
-      }
-    }
-    return Promise.reject(error);
+// Set up request interceptor for CSRF token & Authorization header
+const setRequestHeaders = (config) => {
+  const csrftoken = getCSRFToken();
+  if (csrftoken) {
+    config.headers['X-CSRFToken'] = csrftoken;
   }
-);
 
-// Function to refresh authentication token
+  const accessToken = Cookies.get('accessToken');
+  if (accessToken) {
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+
+  return config;
+};
+
+axiosReq.interceptors.request.use(setRequestHeaders, (error) => Promise.reject(error));
+axiosRes.interceptors.request.use(setRequestHeaders, (error) => Promise.reject(error));
+
+// Token Refresh Function
 export const refreshToken = async () => {
   try {
-    const response = await axios.post('/dj-rest-auth/token/refresh/', {}, {
-      withCredentials: true,
-    });
-    return response.data;
+    const response = await axios.post('/dj-rest-auth/token/refresh/', {}, { withCredentials: true });
+    const newAccessToken = response.data.access;
+    
+    if (newAccessToken) {
+      Cookies.set('accessToken', newAccessToken, { secure: true, sameSite: 'None' });
+    }
+
+    return newAccessToken;
   } catch (err) {
     console.error('Error refreshing token:', err);
     throw err;
   }
 };
 
+// Token Refresh Interceptor (Handles 401 Errors)
+axiosReq.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newAccessToken = await refreshToken();
+        if (newAccessToken) {
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return axiosReq(originalRequest);
+        }
+      } catch (refreshError) {
+        console.error('Token refresh failed:', refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 // Function to check authenticated user
 export const checkUser = async () => {
   try {
-    const response = await axios.get('/dj-rest-auth/user/', {
-      withCredentials: true,
-    });
+    const response = await axios.get('/dj-rest-auth/user/', { withCredentials: true });
     return response.data;
   } catch (err) {
     console.error('Error checking user:', err);
